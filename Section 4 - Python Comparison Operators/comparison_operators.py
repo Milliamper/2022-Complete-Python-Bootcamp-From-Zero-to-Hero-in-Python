6588d1a8ba892f7c9f4d330f5a75d50f06052a8a
Milliamper <szalaimarci99@gmail.com>
2022-10-06 20:39:54 +0200

    update

---
+++ Section 4 - Python Comparison Operators/comparison_operators.py
@@ -0,0 +1,6 @@
+print("Egyenlőség vizsgálat: ", 2 == 2)
+print("Egyenlőség vizsgálat: ", 2 == 1)
+print("Egyenlőség vizsgálat: ", "Szia" == "szia")
+print("Egyenlőség vizsgálat: ", 2 == '2')
+
+print("Egyenlőség vizsgálat: ", 20 != 2)
